83372b248f3b84f64581572bf7e172c3ed1e4d2e
mostafa <mostafakhoshhal@yahoo.com>
2020-05-26 17:43:11 +0430

    income addded in models and some beautification

--- web/models.py
+++ web/models.py
@@ -1,10 +1,29 @@
+from __future__ import unicode_literals
+
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
+# we create a model here for outcoming money / contains--> text-date-amount-user
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # this function will change the name of table into the exact date and time and amount
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+
+# another model for incoming money
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # this function will change the name of table into the exact date and amount
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
